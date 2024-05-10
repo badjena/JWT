b0ff6a90caecba3fc118eee342e90e7023c6d20c
badjena <suchishree.badjena@qalibre.in>
2024-05-10 11:10:54 +0530

    1st commit

---
+++ JWTToken/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using JWTToken.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTToken.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        [Route("GetData")]
+        public string GetData()
+        {
+            return "Authenticated with JWT";
+        }
+
+        [HttpGet]
+        [Route("Details")]
+        public string Details()
+        {
+            return "Authenticated with JWT";
+        }
+
+        [Authorize]
+        [HttpPost]
+        public string Adduser(Users user)
+        {
+            return "User added with Username" + user.Username;
+        }
+    }
+}
---
+++ JWTToken/Models/Users.cs
@@ -0,0 +1,8 @@
+﻿namespace JWTToken.Models
+{
+    public class Users
+    {
+        public string Username { get; set; }
+        public string password { get; set; }
+    }
+}
